6bd44380a5c6d6e5245f197ae5256f8647670bd0
Jose Andres Hernandez Bustio <jbustio@gmail.com>
2021-11-23 21:03:49 -0600

    Delete check version

--- website_webkul_addons/__init__.py
+++ website_webkul_addons/__init__.py
@@ -22,4 +22,4 @@ def pre_init_check(cr):
     from odoo.exceptions import Warning
     version_info = common.exp_version()
     server_serie =version_info.get('server_serie')
-    if server_serie!='14.0':raise Warning('Module support Odoo series 14.0 found {}.'.format(server_serie))
+    
