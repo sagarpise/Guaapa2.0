2786f2c1c5f6c97fec5e783e93bdb10bac951398
Gerardo <gerardo.mixteco@quadit.mx>
2021-12-02 22:49:38 -0600

    [ADD] website_sale_refresh_ajax: welcome module

---
+++ website_sale_refresh_ajax/controllers/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 - QUADIT, SA DE CV(https://www.quadit.mx)
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import navbar
