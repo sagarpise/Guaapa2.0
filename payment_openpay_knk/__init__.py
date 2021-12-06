3c68018e7354d365d0088f662a529b482f98b33b
Jose Andres Hernandez Bustio <jbustio@gmail.com>
2021-12-05 18:45:26 -0600

    Fix openpay module

--- payment_openpay_knk/__init__.py
+++ payment_openpay_knk/__init__.py
@@ -4,10 +4,11 @@
 
 from . import models
 from . import controllers
+#from odoo.addons.payment.models.payment_acquirer import create_missing_journal_for_acquirers
 from odoo.addons.payment import reset_payment_acquirer
 
+
 def uninstall_hook(cr, registry):
-    
     reset_payment_acquirer(cr, registry, 'openpay_card')
     reset_payment_acquirer(cr, registry, 'openpay_store')
     reset_payment_acquirer(cr, registry, 'openpay_bank')
